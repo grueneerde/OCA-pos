8206a2298a1323dcf71d9e610402ac770e6d73cf
SiesslPhillip <phillip.siessl@grueneerde.com>
2025-12-19 08:57:42 +0000

    [t9158] use mounted

--- pos_lot_selection/static/src/js/EditListPopup.js
+++ pos_lot_selection/static/src/js/EditListPopup.js
@@ -14,11 +14,6 @@ odoo.define("pos_lot_selection.EditListPopup", function (require) {
                 super(...arguments);
                 if (this.props.title === this.env._t("Lot/Serial Number(s) Required")) {
                     this.props.lots = this.env.session.lots;
-                    if (this.props.lots.length) {
-                        if (!this.props.list && !this.props.list[0].text) {
-                            this.props.list[0].text = this.props.lots[0];
-                        }
-                    }
                 }
             }
             mounted() {
